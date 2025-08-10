fcb03d080b711d31d681bec0c75bef1a3c95b993
Lee <tunglb94@gmail.com>
2025-08-10 15:26:58 +0700

    Configure settings for production and fix static files

--- bjf_project/settings.py
+++ bjf_project/settings.py
@@ -11,33 +11,38 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a#y(w2-!9z@$p!#^n&!_y_q*!@s#d$f%g&h*j(k)l'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-for-local-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',  # Thư viện giao diện admin mới, phải đặt ở đầu
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize', # Thư viện định dạng số
-    'solo',                    # Thư viện cho cài đặt chung
-    'store.apps.StoreConfig',  # App chính của chúng ta
-    'ckeditor',                # Thư viện trình soạn thảo nội dung
-    'django_group_by',         # Thư viện groupby cho template
+    'django.contrib.humanize',
+    'solo',
+    'store.apps.StoreConfig',
+    'ckeditor',
+    'django_group_by',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Thêm Whitenoise
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -59,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'store.context_processors.site_settings', # Để dùng biến {{ settings }} ở mọi nơi
+                'store.context_processors.site_settings',
             ],
         },
     },
@@ -83,18 +88,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # ... (giữ nguyên phần này)
 ]
 
 
@@ -102,11 +96,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'vi'
-
 TIME_ZONE = 'Asia/Ho_Chi_Minh'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
@@ -114,16 +105,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Dòng này để Django tìm thấy thư mục static của bạn trong lúc phát triển (local)
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+# Dòng này chỉ định nơi collectstatic sẽ gom tất cả file tĩnh vào
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Cấu hình để Whitenoise nén và phục vụ file tĩnh hiệu quả
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Cấu hình cho việc tải lên file (hình ảnh sản phẩm, bài viết)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Cấu hình cho việc tải lên file (hình
